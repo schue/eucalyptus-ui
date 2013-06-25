48e7ddd574d008e9e93a7f9a4c5e3886bd8b5a9a
Ean Schuessler <ean@brainfood.com>
2013-06-25 17:16:02 -0500

    Initial work on native search

--- static/js/models/images.js
+++ static/js/models/images.js
@@ -1,9 +1,26 @@
 define([
     'models/eucacollection',
-    'models/image'
-], function(EucaCollection, Image) {
+    'models/image',
+    'workers/search',
+], function(EucaCollection, Image, Search) {
     var Images = EucaCollection.extend({
 	    model: Image,
+        filters: [
+            {
+                name: 'architecture',
+                label: 'Architecture',
+                type: Search.LIST_MATCH,
+                values: [
+                    {label: 'i386', value: 'i386'}, 
+                    {label: 'x86_64', value: 'x86_64'}
+                ]
+            },
+            {
+                name: 'description',
+                label: 'Description',
+                type: Search.STRING_MATCH
+            },
+        ],
 	    url: '/ec2?Action=DescribeImages',
     });
     return Images;
---
+++ static/js/workers/search.js
@@ -0,0 +1,11 @@
+define([
+], function() {
+    var Search;
+    Search = {
+        STRING_MATCH: 'STRING_MATCH',        
+        BOOLEAN_MATCH: 'BOOLEAN_MATCH',        
+        INTEGER_MATCH: 'INTEGER_MATCH',        
+        LIST_MATCH: 'LIST_MATCH',        
+    }
+    return Search;
+});
